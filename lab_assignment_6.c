c93d5a2224307a1b3483dd82cc08548ed2db5adb
Daniela Contreras <itsdanicdp@gmail.com>
2024-02-22 16:06:53 -0500

    Commit

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -2,8 +2,27 @@
 
 int search(int numbers[], int low, int high, int value) 
 {
-	return -1;
+     int mid = (low + (high-low)/2);
+	while(low > high)
+	{
+		return -1;
+	}
+	if(numbers[mid] == value)
+	{
+		return mid;
+	}
+	else if(numbers[mid] > value)
+	{
+		return search(numbers, low, mid - 1, value);
+	}
+	else if(numbers[mid] < value)
+	{
+		return search(numbers, mid + 1, high, value);
+	}
+	return 0;
 }
+
+	
 
 void printArray(int numbers[], int sz)
 {
